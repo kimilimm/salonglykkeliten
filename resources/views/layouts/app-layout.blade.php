6cf7abea7c114359f73871449dff5e1d04d5e19a
kimilimm <kim@hoffet.net>
2022-02-23 19:28:47 +0100

    removed syntax bug from layout

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     @yield('title')
-    @yield('seo');
+    @yield('seo')
     <meta property="og:type" content="website">
     <meta content="summary_large_image" name="twitter:card">
     <meta content="width=device-width, initial-scale=1" name="viewport">
@@ -53,7 +53,6 @@
 </head>
 <body class="body" id="salong">
     @yield('content')
-
     <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=620c28c9ea080a1e8139270c"
         type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
     </script>
